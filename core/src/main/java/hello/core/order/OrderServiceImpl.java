c9fe6f7f82cdef675424706892b8cc4989df2da4
sangkwon <zndwlrnjr26@daum.net>
2023-08-21 18:01:05 +0900

    :recycle: Refactor AppConfig

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,38 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 추상(인터페이스)DiscountPolicy뿐만 아니라 구체(구현)클래스FixDiscountPolicy에도 의존하고 있다. -> DIP 위반
+     * private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+     * FixDiscountPolicy -> RateDiscountPolicy -> OCP 위반
+     * private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+     * */
+
+    /**
+     * DIP를 지키면?
+     */
+//    private final DiscountPolicy discountPolicy;
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
 
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        
+
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
